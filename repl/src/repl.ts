a4f5845356496ed16b46e2fb2d96f42eb8cbed08
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-08-13 17:23:36 +0200

    repl rough testing

--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,4 +1,17 @@
 import josmInterpolateString from "./../../app/src/josmInterpolateString"
 //const testElem = document.querySelector("#test")
 
-josmInterpolateString()
+const ecosystemConfigJsTemplate = `module.exports = {
+  apps : [{
+    script: "replServer/dist/server.js",
+    name: "$[ branch / hash ].$[ name ]",
+    exec_mode : "cluster",
+    instances: 2,
+    wait_ready: true,
+    args: "--port $[ port ]"
+  }]
+}
+`
+
+
+josmInterpolateString(ecosystemConfigJsTemplate, {branch: "BRANCH", name: "NAME", port: "PORT", hash: "HASH"}).get(console.log)
